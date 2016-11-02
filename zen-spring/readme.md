606f4a3b463d237659fa985297a96fb3ba6e6c2e
pine <916931772@qq.com>
2016-11-02 23:36:17 +0800

    zen-spring v1.3 重构模块

---
+++ zen-spring/readme.md
@@ -0,0 +1,13 @@
+WEB框架简单示例
+============================
+
+### 项目架构
+- spring jdbc
+- spring boot
+- spring security
+- srping session
+- spring cache
+- spring mvc
+
+
+
---
+++ zen-spring/spring-boot/src/main/java/com/zengs/webapp/Application.java
@@ -0,0 +1,24 @@
+package com.zengs.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@SpringBootApplication
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("spring.devtools.restart.enabled", "true");
+        SpringApplication.run(Application.class, args);
+    }
+}
