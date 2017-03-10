dc85ed3b9105df6f83ce62312738b494cd1b28bf
songz <916931772@qq.com>
2017-03-10 14:22:14 +0800

    graphql

---
+++ graphql/src/main/java/com/zengs/entity/User.java
@@ -0,0 +1,52 @@
+package com.zengs.entity;
+
+
+public class User {
+    private int id;
+    private int age;
+    private int sex;
+    private String name;
+    private String pic;
+    // getter, setter...
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+}
--- graphql/src/main/java/com/zengs/graphql/Main.java
+++ graphql/src/main/java/com/zengs/graphql/Main.java
@@ -1,21 +1,107 @@
 package com.zengs.graphql;
 
-/**
- * Created by song on 2017/3/9.
- */
+import graphql.GraphQL;
+import graphql.schema.*;
+
+import java.util.Map;
+
+import static graphql.Scalars.GraphQLBoolean;
+import static graphql.Scalars.GraphQLString;
+import static graphql.schema.GraphQLEnumType.newEnum;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLInputObjectField.newInputObjectField;
+import static graphql.schema.GraphQLInputObjectType.newInputObject;
+import static graphql.schema.GraphQLInterfaceType.newInterface;
+import static graphql.schema.GraphQLObjectType.newObject;
+
 public class Main {
 
     public static void main(String[] args) {
 
         GraphQLObjectType queryType = newObject()
                 .name("helloWorldQuery")
-                .field(field -> field.type(GraphQLString)
-                        .name("hello")
-                        .argument(argument -> argument.name("arg")
-                                .type(GraphQLBoolean))
-                        .dataFetcher(env -> "hello"))
+                .field(newFieldDefinition().type(GraphQLString).name("hello").staticValue("world").description(""))
                 .build();
 
+        GraphQLSchema schema = GraphQLSchema.newSchema()
+                .query(queryType)
+                .build();
+        Map<String, Object> result = (Map<String, Object>) new GraphQL(schema).execute("{hello}").getData();
+
+        System.out.println(result);
+        // Prints: {hello=world}
+
+        new Thread(()->{
+
+        }).start();
     }
+
+    public void objectType(){
+        GraphQLObjectType simpsonCharacter = newObject()
+                .name("SimpsonCharacter")
+                .description("A Simpson character")
+                .field(newFieldDefinition()
+                        .name("name")
+                        .description("The name of the character.")
+                        .type(GraphQLString))
+                .field(newFieldDefinition()
+                        .name("mainCharacter")
+                        .description("One of the main Simpson characters?")
+                        .type(GraphQLBoolean))
+                .build();
+    }
+
+    public void interfaceType(){
+        GraphQLInterfaceType comicCharacter = newInterface()
+                .name("ComicCharacter")
+                .description("A abstract comic character.")
+                .field(newFieldDefinition()
+                        .name("name")
+                        .description("The name of the character.")
+                        .type(GraphQLString))
+                .build();
+    }
+
+
+    /*public void unionType(){
+        GraphQLUnionType PetType = newUnionType()
+                .name("Pet")
+                .possibleType(CatType)
+                .possibleType(DogType)
+                .typeResolver(new TypeResolver() {
+                    @Override
+                    public GraphQLObjectType getType(Object object) {
+                        if (object instanceof Cat) {
+                            return CatType;
+                        }
+                        if (object instanceof Dog) {
+                            return DogType;
+                        }
+                        return null;
+                    }
+                }).build();
+    }*/
+
+    public void enumType(){
+        GraphQLEnumType colorEnum = newEnum()
+                .name("Color")
+                .description("Supported colors.")
+                .value("RED")
+                .value("GREEN")
+                .value("BLUE")
+                .build();
+    }
+
+    public void inputObjectType(){
+        GraphQLInputObjectType inputObjectType = newInputObject()
+                .name("inputObjectType")
+                .field(newInputObjectField()
+                        .name("field")
+                        .type(GraphQLString))
+                .build();
+    }
+
+
+
 
 }
